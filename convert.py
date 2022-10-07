e494022c58c637dc1102bbc8615772a6519dc6b5
Sten Kuusk <sten.kuusk@voco.ee>
2022-10-07 13:58:05 +0300

    Ülesanne: Arvude teisendamine kümnendsüsteemist kahendsüsteemi

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
+''''
 number_as_string = input("Enter a decimal number:")
-#number_as_string = input("tere")
+# decimal -> binary
 
 # input() gives us text, let's convert it to number
 number = int(number_as_string)
@@ -14,3 +15,18 @@ while number > 0:
     number = number // 2
 
 print("Result in binary:", result)
+'''
+# binary -> decimal
+binary_as_string = input("Enter a binary number:")
+print(binary_as_string)
+binarycount = len(binary_as_string)
+print()
+decimal_number = 0
+pow_value = 0
+while(binarycount >= 0):
+    print(binary_as_string[binarycount-1])
+    decimal_number += pow(2, pow_value) * int(binary_as_string[binarycount -1])
+    binarycount -= 1
+    pow_value += 1
+
+    print("Result in decimal:", decimal_number)
