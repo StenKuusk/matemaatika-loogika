10e3d8c35951ddeda3f7d8199bc6dfe853ec2a9c
Sten Kuusk <sten.kuusk@voco.ee>
2022-10-12 13:41:18 +0300

    Ülesanne: Muutuja

---
+++ variables.py
@@ -0,0 +1,8 @@
+word = "spider"
+thousand = "1000"
+ten = 10
+first_half = "full"
+second_half = "moon"
+complete = first_half + " " + second_half
+print(complete)
+print("* {0} {1} *".format(first_half, second_half))
